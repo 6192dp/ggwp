43201a67c7c0605339776f66229d4f15377f02b3
dp6192 <prasad.mvss1@gmail.com>
2022-12-12 16:42:48 +0530

    Add tailwind to the project; Add app colors to tailwind config

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    colors: {
+      transparent: 'transparent',
+      backgroundColor: '#16142B',
+      'white': '#ffffff',
+      'secondayWhite': '#E4E7EC',
+      'primaryBlue': '#1967FF',
+      'secondayBlue': '#4785FF'
+    },
+    extend: {},
+  },
+  plugins: [],
+}
